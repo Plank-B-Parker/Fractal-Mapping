07c8b6d683fa29d46ef7614a7d20cc941b73ab0f
Ibraheem Khan <meeharbi_noiz@outlook.com>
2023-02-14 00:40:16 +0000

    add stuff back

---
+++ src/Input/Mouse.cpp
@@ -0,0 +1,43 @@
+#include "Input/Mouse.h"
+#include "Math/Complex.h"
+#include <iostream>
+
+complex mouse::pos;
+float mouse::scroll = 1;
+bool mouse::clicked;
+float mouse::clickDecay;
+
+extern int WINDOW_HEIGHT, WINDOW_WIDTH;    //global in main
+
+void mouse::mouse_callback(GLFWwindow* window, double xpos, double ypos){
+
+    float centreX = WINDOW_WIDTH/2.0f;
+    float centreY = WINDOW_HEIGHT/2.0f;
+
+    float diffX = xpos - centreX;
+    float diffY = ypos - centreY;
+
+    pos = complex(diffX / (float)centreY, -diffY / (float)centreY);
+
+    scroll = 0;
+
+}
+
+void mouse::mouse_button_callback(GLFWwindow* window, int button, int action, int mods){
+    if(button == GLFW_MOUSE_BUTTON_LEFT){
+        clicked = GLFW_PRESS == action;
+        clickDecay = 1;
+    }
+    
+}
+
+void mouse::mouse_scroll_callback(GLFWwindow* window, double scrollX, double scrollY){
+    scroll = scrollY;
+    if(-0.1f <= scroll && scroll <= 0.1f)
+        scroll = 0.0f;
+}
+
+void mouse::update(float dt){
+    if(!clicked)
+        clickDecay *= (1 - 0.3 * dt);
+}
---
+++ src/Logger.h
@@ -0,0 +1,8 @@
+#pragma once
+#include <iostream>
+#include <string>   //how to declare this class instead of including it?
+
+struct complex;
+
+void log(std::string);
+void log(std::string, complex);
